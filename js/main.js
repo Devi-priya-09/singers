3a38acd146c3c85b178534927e864cc8f7efcafb
Devi-priya-09 <devidevdp.09@gmail.com>
2024-11-15 14:40:37 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function sample1(){
+    console.log('test1')
+    document.getElementById('demo').innerHTML='evening';
+
+}
+function sample2(){
+    console.log('test2')
+    document.getElementById('demo').innerHTML='morning';
+    
+}
---
+++ js/main1.js
@@ -0,0 +1,35 @@
+function view() {
+    console.log('Photo view triggered');
+    document.getElementById('gv').src = "./images/gv1.jpg";
+
+}
+
+  
+function views() {
+    console.log('Photo view triggered');
+    document.getElementById('ani').src = "./images/ani.jpg";
+
+}
+
+
+ function viewsss() {
+        console.log('Photo view triggered');
+        document.getElementById('hip').src = "./images/hihop.jpg";
+ }
+function viewssss() {
+            console.log('Photo view triggered');
+            document.getElementById('sid').src = "./images/siner1.jpg";
+        
+    
+
+  }
+
+  function viewsssss() {
+    console.log('Photo view triggered');
+    document.getElementById('vij').src = "./images/viay.jpg";
+
+
+
+}
+
+  
